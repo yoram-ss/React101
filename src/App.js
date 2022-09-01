5820d54e89e5593f1048654be2e1bb4ffff7d271
Yoram <yoram_ss@softwareseni.com>
2022-09-01 16:25:52 +0700

    Task 7 React Context

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import style from "./mystyle.module.css";
 import Footer from "./Footer";
+import { ThemeContext, themes } from "./contexts/ThemeContext";
 
 function App() {
   const [name, setName] = useState("...");
   const [count, setCount] = useState(0);
+  const [theme, setTheme] = useState(themes.dark);
   // const countme = 0;
 
   useEffect(() => {
@@ -14,23 +16,25 @@ function App() {
   });
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>
-            Hello my name is {name} {count}
-          </code>
-        </p>
-        <button className={style.btnGreen} onClick={() => setName("yoram")}>
-          Change Name
-        </button>
-        <button onClick={() => setCount((prev) => prev + 1)}>+</button>
-        <button onClick={() => setCount((prev) => prev - 1)}>-</button>
-        {/* <button onClick={() => countme + 1}>update var</button> */}
-      </header>
-      <Footer name={name} />
-    </div>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <code>
+              Hello my name is {name} {count}
+            </code>
+          </p>
+          <button className={style.btnGreen} onClick={() => setName("yoram")}>
+            Change Name
+          </button>
+          <button onClick={() => setCount((prev) => prev + 1)}>+</button>
+          <button onClick={() => setCount((prev) => prev - 1)}>-</button>
+          {/* <button onClick={() => countme + 1}>update var</button> */}
+        </header>
+        <Footer name={name} />
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,23 @@
+import { useContext } from "react";
+import { ThemeContext, themes } from "./contexts/ThemeContext";
+
 const Footer = ({ name }) => {
+  const { theme, setTheme } = useContext(ThemeContext);
+
   return (
-    <div>
-      {name && `My name is ${name}, and `}
-      today I'm learning about react
-    </div>
+    <>
+      <div style={{ backgroundColor: theme.background }}>
+        {name && `My name is ${name}, and `}
+        today I'm learning about react
+      </div>
+      <button
+        onClick={() =>
+          setTheme(theme === themes.dark ? themes.light : themes.dark)
+        }
+      >
+        Change background
+      </button>
+    </>
   );
 };
 
